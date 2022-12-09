dcd92d0cee4e8beb8bbcdef8f968c085af9cf67d
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-12-10 01:38:20 +0200

    made modules

---
+++ facedetection/pipeline/__init__.py
@@ -0,0 +1,7 @@
+from . import classifier
+from . import feature
+from . import rectangle
+from . import stage
+from . import cascade_classifier
+
+__all__ = ['classifier','feature','stage','cascade_classifier']
