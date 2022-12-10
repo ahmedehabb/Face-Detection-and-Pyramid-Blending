cd499ac87bcac955c6aeac6c92e1131ab3ece1c6
ahmedehabb <Ahmed.bahloul00@eng-st.cu.edu.eg>
2022-12-10 02:26:10 +0200

    added rectangle in module

--- facedetection/pipeline/__init__.py
+++ facedetection/pipeline/__init__.py
@@ -4,4 +4,4 @@ from . import rectangle
 from . import stage
 from . import cascade_classifier
 
-__all__ = ['classifier','feature','stage','cascade_classifier']
+__all__ = ['classifier','feature','stage','rectangle','cascade_classifier']
